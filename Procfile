221228bcc4c872e5b93071c45982f7fb37396aa2
YairGM <yagamo@gmail.com>
2018-05-01 18:47:32 -0500

    new archives

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn perrop.wsgi
---
+++ parrandero/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/yair/development/perro/parrandero/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
